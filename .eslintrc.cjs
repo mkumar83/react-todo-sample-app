64aa61defcb51a49b79b0b27fc4e6fceb74bb74d
mchinnu <muthukumar.chinnu@amadeus.com>
2022-08-29 12:49:05 +0530

    chore: added eslint to lint the typescript and javascript files

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'jest'],
+    root: true,
+    ignorePatterns: ['build', 'node_modules', '*.config.js'],
+    overrides: [
+        {
+          files: ['**/*.test.js'],
+          env: { 'jest/globals': true },
+          plugins: ['jest'],
+          extends: ['plugin:jest/recommended'],
+        },
+    ],
+};
