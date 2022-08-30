90396fb2c0b7aec04d4a24dfaa8246b9a40cb5e8
mchinnu <muthukumar.chinnu@amadeus.com>
2022-08-30 22:27:40 +0530

    chore: removed test coverage to lint staged

--- lint-staged.js
+++ lint-staged.js
@@ -4,7 +4,6 @@ module.exports = {
     '*.{js,jsx,ts,tsx}': [
         'eslint --max-warnings=0',
         'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-        'react-scripts test --coverage --watchAll=false --findRelatedTests --passWithNoTests',
         () => 'tsc-files --noEmit',
     ],
     // eslint-disable-next-line sort-keys
