9bb859f744188495da051666d310dd4cc8c0a1ff
mchinnu <muthukumar.chinnu@amadeus.com>
2022-09-02 12:33:25 +0530

    chore: restored the component to right state

--- src/App.test.js
+++ src/App.test.js
@@ -15,33 +15,3 @@ test('renders Integrated CI', () => {
     const linkElement = screen.getByText(/Integrated CI/i)
     expect(linkElement).toBeInTheDocument()
 })
-
-test('renders Lint Staging', () => {
-    render(<App />)
-    const linkElement = screen.getByText(/Lint Staging/i)
-    expect(linkElement).toBeInTheDocument()
-})
-
-test('renders Coverage', () => {
-    render(<App />)
-    const linkElement = screen.getByText(/Coverage/i)
-    expect(linkElement).toBeInTheDocument()
-})
-
-test('renders Prettier', () => {
-    render(<App />)
-    const linkElement = screen.getByText(/Prettier/i)
-    expect(linkElement).toBeInTheDocument()
-})
-
-test('renders ESlint', () => {
-    render(<App />)
-    const linkElement = screen.getByText(/ESlint/i)
-    expect(linkElement).toBeInTheDocument()
-})
-
-test('renders Jest Test Again New', () => {
-    render(<App />)
-    const linkElement = screen.getByText(/Jest Test Again New/i)
-    expect(linkElement).toBeInTheDocument()
-})
--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ const App = () => (
     <div>
         <div>Hello World!</div>
         <div>Integrated CI</div>
-        <div>Lint Staging</div>
-        <div>Coverage</div>
-        <div>Prettier</div>
-        <div>ESlint</div>
-        <div>Jest Test Again New</div>
     </div>
 )
 
